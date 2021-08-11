7810d526310033c4b6b48851908720c4c4ebf649
Yousif Abozid <yousif.abozid@yahoo.com>
2021-08-12 00:31:26 +0200

    add user marker on the map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,6 +1,9 @@
-// import { User } from "./User";
-// import { Company } from "./Company";
-import {} from "google.maps";
+import { User } from "./User";
+import { Company } from "./Company";
+import {} from "google.maps"; // you should import google.maps like this so you can use namespace google
 import { CustomMap } from "./CustomMap";
 
-new CustomMap("map");
+const user = new User();
+const customMap = new CustomMap("map");
+
+customMap.addUserMarker(user);
