bc1736a66dab7d9eee13049381ece0df7fb8b693
Yousif Abozid <yousif.abozid@yahoo.com>
2021-08-12 06:33:05 +0200

    great refactor of sort algorithm

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,33 +1,9 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                // All of this only works if collection is number[]
-                // If collection is an Array of numbers, so this logic:
-                // below is a type guard
-                if (this.collection instanceof Array) {
-                    // collection number[]
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        var leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                // Only going to work if collection is string
-                // If collection is a string, do this logic instead:
-                // below another type guard but only works with string, number and boolean
-                if (typeof this.collection === "string") {
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
-var sorter = new Sorter([10, 3, -5, 0]);
+Object.defineProperty(exports, "__esModule", { value: true });
+var sorter_1 = require("./sorter");
+var NumbersCollection_1 = require("./NumbersCollection");
+var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+console.log(numbersCollection.data);
+var sorter = new sorter_1.Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,33 +1,9 @@
-class Sorter {
-    constructor(public collection: number[] | string) {}
+import { Sorter } from "./sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-    sort(): void {
-        const { length } = this.collection;
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+console.log(numbersCollection.data);
 
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                // All of this only works if collection is number[]
-                // If collection is an Array of numbers, so this logic:
-                // below is a type guard
-                if (this.collection instanceof Array) {
-                    // collection number[]
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-
-                // Only going to work if collection is string
-                // If collection is a string, do this logic instead:
-                // below another type guard but only works with string, number and boolean
-                if (typeof this.collection === "string") {
-                }
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
