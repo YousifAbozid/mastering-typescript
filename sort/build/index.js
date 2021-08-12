b3c57671c74c947addc0f227d933bfa4231d4f81
Yousif Abozid <yousif.abozid@yahoo.com>
2021-08-13 01:06:06 +0200

    do some tests

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,23 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var NumbersCollection_1 = require("./NumbersCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
+var LinkedList_1 = require("./LinkedList");
 // Sorting an array of numbers below:
 var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
 // Sorting string below:
-// const charactersCollection = new CharactersCollection("YouSif");
-// console.log(charactersCollection.data);
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersCollection("YouSif");
+charactersCollection.sort();
+console.log(charactersCollection.data);
 // Sort linked list below:
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-10);
-// linkedList.add(-3);
-// linkedList.add(4);
-// const sorter = new Sorter(linkedList);
-// linkedList.print();
-// sorter.sort();
-// linkedList.print();
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,4 +1,3 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
@@ -9,19 +8,15 @@ numbersCollection.sort();
 console.log(numbersCollection.data);
 
 // Sorting string below:
-// const charactersCollection = new CharactersCollection("YouSif");
-// console.log(charactersCollection.data);
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection("YouSif");
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
 // Sort linked list below:
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-10);
-// linkedList.add(-3);
-// linkedList.add(4);
-// const sorter = new Sorter(linkedList);
-// linkedList.print();
-// sorter.sort();
-// linkedList.print();
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();
