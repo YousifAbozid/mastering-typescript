ed0a1fe752048a4a7fc96bfaa6388f3b80dcc62e
Yousif Abozid <yousif.abozid@yahoo.com>
2021-08-12 04:34:46 +0200

    initial sort algorithm

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log(123);
+var Sorter = /** @class */ (function () {
+    function Sorter(colllection) {
+        this.colllection = colllection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.colllection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.colllection[j] > this.colllection[j + 1]) {
+                    var leftHand = this.colllection[j];
+                    this.colllection[j] = this.colllection[j + 1];
+                    this.colllection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.colllection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,21 @@
-console.log(123);
+class Sorter {
+    constructor(public colllection: number[]) {}
+
+    sort(): void {
+        const { length } = this.colllection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.colllection[j] > this.colllection[j + 1]) {
+                    const leftHand = this.colllection[j];
+                    this.colllection[j] = this.colllection[j + 1];
+                    this.colllection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.colllection);
