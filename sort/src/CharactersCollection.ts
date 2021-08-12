af635aa88c4f68a4fcbacdb4376c6c827748b61e
Yousif Abozid <yousif.abozid@yahoo.com>
2021-08-12 07:33:49 +0200

    add length and compare to characters collection class

---
+++ sort/src/CharactersCollection.ts
@@ -0,0 +1,14 @@
+export class CharactersCollection {
+    constructor(public data: string) {}
+
+    get length(): number {
+        return this.data.length;
+    }
+
+    compare(leftIndex: number, rightIndex: number): boolean {
+        return (
+            this.data[leftIndex].toLowerCase() >
+            this.data[rightIndex].toLowerCase()
+        );
+    }
+}
