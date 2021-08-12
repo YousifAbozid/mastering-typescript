8f83d4cea095a86e7f17e2aac6cc00a23e6b5263
Yousif Abozid <yousif.abozid@yahoo.com>
2021-08-12 05:26:27 +0200

    add some notes to sort algorithm

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,16 +1,27 @@
 "use strict";
 var Sorter = /** @class */ (function () {
-    function Sorter(colllection) {
-        this.colllection = colllection;
+    function Sorter(collection) {
+        this.collection = collection;
     }
     Sorter.prototype.sort = function () {
-        var length = this.colllection.length;
+        var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.colllection[j] > this.colllection[j + 1]) {
-                    var leftHand = this.colllection[j];
-                    this.colllection[j] = this.colllection[j + 1];
-                    this.colllection[j + 1] = leftHand;
+                // All of this only works if collection is number[]
+                // If collection is an Array of numbers, so this logic:
+                // below is a type guard
+                if (this.collection instanceof Array) {
+                    // collection number[]
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // Only going to work if collection is string
+                // If collection is a string, do this logic instead:
+                // below another type guard but only works with string, number and boolean
+                if (typeof this.collection === "string") {
                 }
             }
         }
@@ -19,4 +30,4 @@ var Sorter = /** @class */ (function () {
 }());
 var sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
-console.log(sorter.colllection);
+console.log(sorter.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,15 +1,27 @@
 class Sorter {
-    constructor(public colllection: number[]) {}
+    constructor(public collection: number[] | string) {}
 
     sort(): void {
-        const { length } = this.colllection;
+        const { length } = this.collection;
 
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.colllection[j] > this.colllection[j + 1]) {
-                    const leftHand = this.colllection[j];
-                    this.colllection[j] = this.colllection[j + 1];
-                    this.colllection[j + 1] = leftHand;
+                // All of this only works if collection is number[]
+                // If collection is an Array of numbers, so this logic:
+                // below is a type guard
+                if (this.collection instanceof Array) {
+                    // collection number[]
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+
+                // Only going to work if collection is string
+                // If collection is a string, do this logic instead:
+                // below another type guard but only works with string, number and boolean
+                if (typeof this.collection === "string") {
                 }
             }
         }
@@ -18,4 +30,4 @@ class Sorter {
 
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
-console.log(sorter.colllection);
+console.log(sorter.collection);
