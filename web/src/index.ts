c8a5efa374631f85beba027c3315692c2a7e933e
Yousif Abozid <yousif.abozid@yahoo.com>
2021-08-15 18:29:26 +0200

    update user data

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,8 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "Yousif", age: 22 });
+
+user.set({ name: "Yoyo", age: 20 });
+
 console.log(user.get("name"));
 console.log(user.get("age"));
